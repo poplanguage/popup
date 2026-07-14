97cb8bdfc0d29e2b7944d8f7d3630c964721bfd3
GalactHD <139361517+GalactHD@users.noreply.github.com>
2026-07-14 11:59:16 -0300

    feat: toolchains cli command

--- src/popup/cli/main.cr
+++ src/popup/cli/main.cr
@@ -1,10 +1,12 @@
 require "option_builder"
 require "./install"
+require "./toolchains"
 
 module Popup::CLI
   def self.setup
     command = OptionBuilder.command("popup", "The Pop toolchain manager tool") do |cmd|
       Install.register(cmd)
+      Toolchains.register(cmd)
     end
 
     command.execute
