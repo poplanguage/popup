d3d5c178b4bf7ab6a8c956b069a534feb5fecb1a
GalactHD <139361517+GalactHD@users.noreply.github.com>
2026-07-14 13:26:01 -0300

    refactor: using glob imports for require all modules

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,10 +4,7 @@ require "json"
 require "crest"
 require "file_utils"
 
-require "../src/popup/version"
-require "../src/popup/github"
-require "../src/popup/utils/target"
-require "../src/popup/installer"
+require "../src/popup"
 
 include Popup
 
