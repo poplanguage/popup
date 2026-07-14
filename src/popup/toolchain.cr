ca3ea5662dde913b938da638371d8d70711e120e
GalactHD <139361517+GalactHD@users.noreply.github.com>
2026-07-14 11:29:00 -0300

    feat: add Toolchain class with DI and rustup-like approach

---
+++ src/popup/toolchain.cr
@@ -0,0 +1,57 @@
+class Popup::Toolchain
+  DEFAULT_BASE_DIR = File.join(ENV["HOME"], ".popup")
+
+  getter base_dir : String
+  getter bin_dir : String
+  getter toolchains_dir : String
+  getter shim_path : String
+
+  def initialize(@base_dir = ENV.fetch("POPUP_HOME", DEFAULT_BASE_DIR))
+    @bin_dir = File.join(@base_dir, "bin")
+    @toolchains_dir = File.join(@base_dir, "toolchains")
+    @shim_path = File.join(@bin_dir, "pop")
+  end
+
+  def versions : Array(String)
+    return [] of String unless Dir.exists?(@toolchains_dir)
+
+    Dir.children(@toolchains_dir)
+      .select(&.starts_with?("v"))
+      .sort!
+  end
+
+  def active_version : String
+    File.basename(File.readlink(default_link))
+  end
+
+  def installed?(version : String) : Bool
+    Dir.exists?(File.join(@toolchains_dir, version))
+  end
+
+  def set_active(version : String) : Nil
+    Dir.mkdir_p(@toolchains_dir)
+    File.delete(default_link) if File.symlink?(default_link)
+    File.symlink(version, default_link)
+  end
+
+  def install(version : String, target : String) : Nil
+    set_active(version)
+    write_pop_shim(target)
+  end
+
+  def write_pop_shim(target : String) : Nil
+    Dir.mkdir_p(@bin_dir)
+
+    content = <<-SHIM
+    #!/usr/bin/env bash
+    exec "$HOME/.popup/toolchains/default/pop-#{target}" "$@"
+    SHIM
+
+    File.write(@shim_path, content)
+    File.chmod(@shim_path, 0o755)
+  end
+
+  private def default_link : String
+    File.join(@toolchains_dir, "default")
+  end
+end
