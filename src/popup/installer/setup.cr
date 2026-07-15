ef3778ae91efd453a67c42d096dbd90485f9d5a3
GalactHD <139361517+GalactHD@users.noreply.github.com>
2026-07-15 10:52:52 -0300

    refactor: using compress/zip over calling unzip via "child process"

--- src/popup/installer/setup.cr
+++ src/popup/installer/setup.cr
@@ -1,26 +1,41 @@
 require "file_utils"
+require "compress/zip"
 
 class Popup::Installer::Setup
   def initialize(@version : String, @archive_path : String, @toolchains_dir : String)
   end
 
-  def run : Nil
+  def run
     version_dir = File.join(@toolchains_dir, @version)
-    tmp = File.tempname("popup")
-    Dir.mkdir_p(tmp)
+    temp_name = File.tempname("popup")
 
-    result = Process.run("unzip", {"-q", "-o", @archive_path, "-d", tmp})
-    unless result.success?
-      File.delete(@archive_path)
-      Dir.delete(tmp)
-      raise "failed to extract #{File.basename(@archive_path)}"
-    end
+    Dir.mkdir_p(temp_name)
+    unzip_file(temp_name)
 
     if Dir.exists?(version_dir)
       FileUtils.rm_rf(version_dir)
     end
 
-    File.rename(tmp, version_dir)
+    File.rename(temp_name, version_dir)
     File.delete(@archive_path)
+  end
+
+  private def unzip_file(temp_name : String)
+    Compress::Zip::File.open(@archive_path) do |file|
+      file.entries.each do |entry|
+        next if entry.dir?
+
+        entry.open do |io|
+          out_path = File.join(temp_name, entry.filename)
+
+          FileUtils.mkdir_p(File.dirname(out_path))
+          File.write(out_path, io.gets_to_end)
+          File.chmod(out_path, 0o755)
+        end
+      end
+    end
+  rescue ex : Compress::Zip::Error
+    FileUtils.rm_rf(temp_name)
+    raise "failed to extract #{File.basename(@archive_path)}: #{ex.message}"
   end
 end
